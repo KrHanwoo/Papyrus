cc1d49c9ac4fcb4c91f5ee6e7b60b7ced5974753
KrHanwoo <koreahan8549@gmail.com>
2023-09-28 12:38:57 +0900

    first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{WindowEvent, Manager};
+
+fn main() {
+    tauri::Builder::default()
+    .setup(|app| {
+        let Some(window) = app.get_window("main") else {
+            return Ok(())
+        };
+
+        window.on_window_event(|event| {
+            match event {
+                WindowEvent::Resized(..) => {
+                    std::thread::sleep(std::time::Duration::from_millis(1))
+                }
+                _ => {}
+            }
+        });
+
+        Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
---
+++ src/main.js
@@ -0,0 +1,101 @@
+const { invoke } = window.__TAURI__.tauri;
+
+// let greetInputEl;
+// let greetMsgEl;
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  // greetInputEl = document.querySelector("#greet-input");
+  // greetMsgEl = document.querySelector("#greet-msg");
+  // document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   greet();
+  // });
+
+  const dropdowns = Array.from(document.getElementsByClassName('dropdown'));
+  dropdowns.forEach(d => {
+    const input = d.querySelector('input');
+    const list = d.querySelector('.list');
+    const options = Array.from(list.querySelectorAll('span'));
+
+    
+
+    input.onfocus = () => {
+      input.placeholder = input.value;
+      input.value = '';
+      options.forEach(x => x.classList.remove('current'));
+
+      list.innerHTML = '';
+      options.forEach(x => list.append(x));
+
+      const selected = list.querySelector('.selected') ?? list.querySelector('.current');
+      selected.classList.add('current');
+      selected.scrollIntoView({ block: 'center' });
+    };
+
+    input.onblur = () => {
+      const selected = list.querySelector('.selected')?.innerText ?? input.placeholder;
+      input.value = selected;
+      input.placeholder = '';
+    }
+
+    input.oninput = () => {
+      const value = input.value;
+      const reg = new RegExp('.*' + value.replace(/(.)/g, "$1.*"));
+      const filtered = options.filter(x => reg.test(x.innerText));
+      list.innerHTML = '';
+      filtered.forEach(x => list.append(x));
+      if (!filtered.some(x => x.classList.contains('current'))){
+        options.forEach(x => x.classList.remove('current'));
+        list.firstElementChild?.classList?.add('current');
+      }
+    };
+
+    input.onkeydown = (e) => {
+      const current = list.querySelector('.current');
+      switch (e.key) {
+        case 'ArrowUp': {
+          e.preventDefault();
+          if (!current) return;
+          current.classList.remove('current');
+          const previous = current.previousElementSibling ?? list.lastElementChild;
+          previous.classList.add('current');
+          previous.scrollIntoView({ block: 'center' });
+          break;
+        }
+        case 'ArrowDown': {
+          e.preventDefault();
+          if (!current) return;
+          current.classList.remove('current');
+          const next = current.nextElementSibling ?? list.firstElementChild;
+          next.classList.add('current');
+          next.scrollIntoView({ block: 'center' });
+          break;
+        }
+        case 'Enter': {
+          e.preventDefault();
+          if (!current) return;
+          options.forEach(x => x.classList.remove('selected'));
+          current.classList.add('selected');
+          input.value = current.innerText;
+          input.blur();
+          break;
+        }
+      }
+    }
+
+    options.forEach(s => {
+      s.addEventListener('mouseover', () => {
+        options.forEach(x => x.classList.remove('current'));
+        s.classList.add('current');
+      });
+      s.onmousedown = () => {
+        options.forEach(x => x.classList.remove('selected'));
+        s.classList.add('selected');
+        input.value = s.innerText;
+      };
+    })
+  });
+});
+
+//https://api.papermc.io/v2/projects/paper
